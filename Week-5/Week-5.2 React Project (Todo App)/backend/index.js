f912aebac1d27f19ea2ba32206cceabe3e7af9c4
Aurojyoti <developeratbackend@gmail.com>
2024-01-04 23:52:59 +0530

    basic zod boiler code updated

--- Week-5/Week-5.2 React Project (Todo App)/backend/index.js
+++ Week-5/Week-5.2 React Project (Todo App)/backend/index.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.json());
 
 app.post("/todo", function(req, res)  {
-
+     
 });
 
 
@@ -14,5 +14,5 @@ app.get("/todos", function(req, res) {
 
 
 app.put("/completed", function(req, res){
-    
+
 })
