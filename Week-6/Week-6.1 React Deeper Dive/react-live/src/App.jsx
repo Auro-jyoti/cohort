82fdf8f6ce8e613696e3b133ea19ae8b3c80776e
Aurojyoti <developeratbackend@gmail.com>
2024-01-06 20:04:25 +0530

    Check git history to go through the code

--- Week-6/Week-6.1 React Deeper Dive/react-live/src/App.jsx
+++ Week-6/Week-6.1 React Deeper Dive/react-live/src/App.jsx
@@ -1,35 +1,50 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
+  // const [title, setTitle] = useState("My name is Goldie")
+
+  // function updateTitle() {
+  //   setTitle("My name is "+ Math.random());
+  // }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <HeaderWithButton />
+      <Header title="test"></Header>
+      <Header title="Aurojyoti"></Header>
+      <Header title="Aurojyoti"></Header>
+      <Header title="Aurojyoti"></Header>
     </>
-  )
+  );
 }
+
+function HeaderWithButton() {
+  const [title, setTitle] = useState("My name is Goldie");
+
+  function updateTitle() {
+    setTitle("My name is " + Math.random());
+  }
+  return (
+    <>
+      <button onClick={updateTitle}>Update the title</button>
+      <Header title={title}></Header>
+      <Header title={title}></Header>
+    </>
+  );
+}
+
+// function Header({title}) {
+//   return <div>
+//     {title}
+//   </div>
+// }
+
+
+const Header = React.memo(function Header({ title }) {
+  return <div>{title}</div>;
+});
 
 export default App
