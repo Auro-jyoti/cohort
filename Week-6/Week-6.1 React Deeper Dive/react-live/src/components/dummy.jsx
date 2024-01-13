551a76365e17ba4fe4452f9a3efb05f8d2fbac24
Aurojyoti <developeratbackend@gmail.com>
2024-01-14 04:54:23 +0530

    Week-6 completed

---
+++ Week-6/Week-6.1 React Deeper Dive/react-live/src/components/dummy.jsx
@@ -0,0 +1,40 @@
+{
+  /*
+        function App() {
+            return (<div>
+                <CardWrapper innerComponent={TextComponent} />
+            </div>)
+        }
+        function CardWrapper({innerComponent}) {
+            // create a div which has a border 
+            // and inside the div, renders the prop
+            return <div style={{something}}>
+                {innerComponent}
+            </div>
+        }
+
+        function TextComponent() {
+            return <div> hi there </div>
+        }
+
+        // actual and most used way 
+        function App() {
+            return <div>
+                <CardWrapper>
+                    hi there
+                </CardWrapper>
+            </div>
+        }
+
+        function CardWrapper({children}) {
+            console.log(children)
+
+            return (
+                <div>
+                    {children}
+                </div>
+            )
+        }
+
+     */
+}
