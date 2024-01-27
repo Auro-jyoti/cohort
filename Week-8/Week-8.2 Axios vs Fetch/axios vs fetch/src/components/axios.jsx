0a604256a31388599567e7dcfa72099d11a09662
Aurojyoti <developeratbackend@gmail.com>
2024-01-27 08:46:14 +0530

    post call with body using fetch

--- Week-8/Week-8.2 Axios vs Fetch/axios vs fetch/src/components/axios.jsx
+++ Week-8/Week-8.2 Axios vs Fetch/axios vs fetch/src/components/axios.jsx
@@ -28,3 +28,21 @@ async function axiosPostCall() {
     const response = await axios.delete("https://sum-server.100xdevs.com/todos");
     console.log(response.data.todos.length);
 }
+
+async function postCallWithBody() {
+    const response = await fetch(
+        "https://www.toptal.com/developers/postbin/1706261117587-5522551864851",
+        {
+            method: "POST",
+            body: {
+                username: "harkirat",
+                password: "123223"
+            },
+            headers: {
+                Authorization: "Bearer 123"
+            }
+        }
+    )
+    const textualData = await response.text();
+    console.log(textualData);
+}
