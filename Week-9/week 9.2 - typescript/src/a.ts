7736787d8350d8e8011ee7fdc524a773d6eea3a7
Aurojyoti <developeratbackend@gmail.com>
2024-01-30 00:08:52 +0530

    interfaces in ts

--- Week-9/week 9.2 - typescript/src/a.ts
+++ Week-9/week 9.2 - typescript/src/a.ts
@@ -3,35 +3,77 @@ let x: number = 1;
 console.log(x);
 
 function greet(firstName: string, lastName: string, age: Number) {
-    console.log("Hello " + firstName);
+  console.log("Hello " + firstName);
 }
 
 greet("Goldie", "lol", 4);
 
 // type inference
 function sum(a: number, b: number) {
-    return a + b;
+  return a + b;
 }
 
 const value = sum(1, 3);
 console.log(value);
 
 function isLegal(age: number): boolean {
-    if(age !<= 18) {
-        return false;
-    }
+  if (age! <= 18) {
+    return false;
+  }
 
-    return true;
+  return true;
 }
 
 const result = isLegal(1);
-console.log(result); 
-
+console.log(result);
 
 function runAfter1S(fn: () => void) {
-    setTimeout(fn, 1000);
+  setTimeout(fn, 1000);
 }
 
-runAfter1S(function(){
-    console.log("Hi There!!");
-})
+runAfter1S(function () {
+  console.log("Hi There!!");
+});
+
+// interfaces :
+
+interface User {
+  firstName: string;
+  lastName: string;
+  age: number;
+  email?: string,
+}
+
+function isLegalObj(user: User) {
+  return !(user.age <= 18);
+}
+
+isLegalObj({
+  firstName: "Goldie",
+  lastName: "lol",
+  age: 20,
+});
+
+interface Person {
+    name: string,
+    age: number,
+    greet(phrase: string): void,
+}
+
+class Employee implements Person {
+    name: string;
+    age: number;
+
+    constructor(n: string, a: number) {
+        this.name = n;
+        this.age = a;
+    }
+
+    greet(phrase: string) {
+        console.log(`${phrase} ${this.name}`);
+    }
+}
+
+const e = new Employee("goldie", 22);
+
+console.log(e.name); 
