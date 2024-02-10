cd4c2e83511e41841834e488a022a5ac6164f768
Aurojyoti <developeratbackend@gmail.com>
2024-02-10 20:49:14 +0530

    done with week 11.1

--- Week-11/week-eleven-hono-app/src/index.ts
+++ Week-11/week-eleven-hono-app/src/index.ts
@@ -2,12 +2,21 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  // body, headers, query parameters, middlewares, connecting to a database
-  // return c.text('Hello Hono!')
-  return c.json({
-    message: "from Hono"
-  })
-})
+// app.get('/', (c) => {
+//   // body, headers, query parameters, middlewares, connecting to a database
+//   // return c.text('Hello Hono!')
+//   return c.json({
+//     message: "from Hono"
+//   })
+// })
+
+app.post("/", async (c) => {
+  const body = await c.req.json();
+  console.log(body);
+  console.log(c.req.header("Authorization"));
+  console.log(c.req.query("params"));
+
+  return c.text("Hello Hono!");
+});
 
 export default app
